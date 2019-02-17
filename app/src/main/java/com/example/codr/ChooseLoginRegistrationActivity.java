9863dae7ba4d874ad16af7a3a5d9c1d8555135c1
cap3k <dylanliner@gmail.com>
2019-02-17 15:22:32 +0100

    chooseregistrationorlogin+registration

---
+++ app/src/main/java/com/example/codr/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,48 @@
+package com.example.codr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.registration);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+
+
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+
+
+        });
+    }
+
+
+
+}
